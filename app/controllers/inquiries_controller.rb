ce59de9633f38610e73a7334d4a3bb469d111d3d
Apprentice <gin3002@gmail.com>
2015-08-24 14:46:02 +1200

    pass04

--- app/controllers/inquiries_controller.rb
+++ app/controllers/inquiries_controller.rb
@@ -1,4 +1,8 @@
 class InquiriesController < ApplicationController
   def new
   end
+
+  def create
+    render text: "Thank you"
+  end
 end
--- spec/features/inquiries_spec.rb
+++ spec/features/inquiries_spec.rb
@@ -7,3 +7,16 @@ RSpec.feature "Inquiries", type: :feature do
     expect(page).to have_selector('form')
   end
 end
+
+
+RSpec.feature "Inquiries", type: :feature do
+  scenario 'user can complete and submit the form' do
+    visit contact_path
+
+    fill_in 'Message', with: "This is my message"
+
+    click_button 'Send message'
+
+    expect(page).to have_content('Thank you')
+  end
+end
