a037aa8482cd27fc49975b0c62821769f4e8613c
Apprentice <gin3002@gmail.com>
2015-08-24 15:30:20 +1200

    pass08

--- app/controllers/inquiries_controller.rb
+++ app/controllers/inquiries_controller.rb
@@ -3,6 +3,14 @@ class InquiriesController < ApplicationController
   end
 
   def create
+    inquiry = Inquiry.new
+    inquiry.name = params[:inquiry][:name]
+    inquiry.email = params[:inquiry][:email]
+    inquiry.subject = params[:inquiry][:subject]
+    inquiry.message = params[:inquiry][:message]
+
+    InquiryMailer.inquiry_email(inquiry).deliver_now
+
     flash.notice = "Thank you for sending us a message."
     redirect_to root_url
   end
